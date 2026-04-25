f036e8dbf9b6042500e443747ca657eb8a317696
kimyoungun90-beep <kim.youngun90@gmail.com>
2026-04-25 14:55:46 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,41 @@
+const CACHE_NAME = "curator-hub-v10";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  const req = event.request;
+
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        return res;
+      })
+      .catch(() => caches.match(req).then(cached => cached || caches.match("./index.html")))
+  );
 });
